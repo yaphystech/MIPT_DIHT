0aebc44ba3e2dbf60329dfff77822a2a3d56e88f
yaphystech <37752053+yaphystech@users.noreply.github.com>
2019-07-04 17:44:53 +0300

    Update Floyd.cpp

--- Floyd.cpp
+++ Floyd.cpp
@@ -4,70 +4,66 @@
 using std::vector;
 using std::pair;
 using std::make_pair;
-
-using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
 
 class Matrixgraph {
 public:
-    /*Matrixgraph(Matrixgraph* unrelaxed_graph){
-        adjacency_matrix = unrelaxed_graph->adjacency_matrix;
-        auto& dist = adjacency_matrix;
-        for(int k = 0; k < VerticesCount(); ++k){
-            for(int i = 0; i < VerticesCount(); ++i){
-                for(int j = 0; j < VerticesCount(); ++j){
-                    dist[i][j] = std::min(dist[i][k] + dist[k][j], dist[i][j]);
-                }
-            }
-        }
-    }*/
-    void transitive_closure(){
-        auto& dist = adjacency_matrix;
-        for(int k = 0; k < VerticesCount(); ++k){
-            for(int i = 0; i < VerticesCount(); ++i){
-                for(int j = 0; j < VerticesCount(); ++j){
-                    dist[i][j] = std::min(dist[i][k] + dist[k][j], dist[i][j]);
-                }
-            }
-        }
-    }
-    ~Matrixgraph() {}
-
-    Matrixgraph(int amount_of_vertices){
-        adjacency_matrix.resize(amount_of_vertices);
-        for(int i = 0; i < amount_of_vertices; ++i){
-            adjacency_matrix[i].assign(amount_of_vertices, INT16_MAX);
-        }
-    }
-
-    void AddEdge(const int from, const int to, const int cost){
-        adjacency_matrix[from][to] = cost;
-    }
-
-    int VerticesCount() const{
-        return adjacency_matrix.size();
-    }
-
-    void GetNextVertices(int vertex, vector<pair<int, int>>& vertices) const {
-        vertices.clear();
-        for(int i = 0; i < VerticesCount(); ++i){
-            if(i != INT16_MAX){
-                vertices.push_back(make_pair(i, adjacency_matrix[vertex][i]));
-            }
-        }
-    }
-
-    void print(){
-        for(int i = 0; i < VerticesCount(); ++i) {
-            for(int j = 0; j < VerticesCount(); ++j) {
-                cout << ((adjacency_matrix[i][j] != INT16_MAX) ? adjacency_matrix[i][j] : 0) << ' ';
-            }
-            cout << endl;
-        }
-    }
-
+    void transitive_closure();
+    ~Matrixgraph() {};
+    Matrixgraph(const int& amount_of_vertices);
+    void AddEdge(const int& from, const int& to, const int& cost);
+    int VerticesCount() const;
+    void GetNextVertices(const int& vertex, vector<pair<int, int>>& vertices) const;
+    void print();
 private:
     vector<vector<int>> adjacency_matrix;
 };
+
+void Matrixgraph::transitive_closure(){
+    auto& dist = adjacency_matrix;
+    for(int k = 0; k < VerticesCount(); ++k){
+        for(int i = 0; i < VerticesCount(); ++i){
+            for(int j = 0; j < VerticesCount(); ++j){
+                dist[i][j] = std::min(dist[i][k] + dist[k][j], dist[i][j]);
+            }
+        }
+    }
+}
+
+Matrixgraph::Matrixgraph(const int& amount_of_vertices){
+    adjacency_matrix.resize(amount_of_vertices);
+    for(int i = 0; i < amount_of_vertices; ++i){
+        adjacency_matrix[i].assign(amount_of_vertices, INT16_MAX);
+    }
+}
+
+void Matrixgraph::AddEdge(const int& from, const int& to, const int& cost){
+    adjacency_matrix[from][to] = cost;
+}
+
+int Matrixgraph::VerticesCount() const{
+    return adjacency_matrix.size();
+}
+
+void Matrixgraph::GetNextVertices(const int& vertex, vector<pair<int, int>>& vertices) const {
+    vertices.clear();
+    for(int i = 0; i < VerticesCount(); ++i){
+        if(i != INT16_MAX){
+            vertices.push_back(make_pair(i, adjacency_matrix[vertex][i]));
+        }
+    }
+}
+
+void Matrixgraph::print(){
+    for(int i = 0; i < VerticesCount(); ++i) {
+        for(int j = 0; j < VerticesCount(); ++j) {
+            cout << ((adjacency_matrix[i][j] != INT16_MAX) ? adjacency_matrix[i][j] : 0) << ' ';
+        }
+        cout << endl;
+    }
+}
 
 int main() {
     int amount_of_vertices;
@@ -82,8 +78,5 @@ int main() {
     }
     graph.transitive_closure();
     graph.print();
-    //Matrixgraph relaxed_graph(graph);
-    //relaxed_graph.print();
-
     return 0;
 }
