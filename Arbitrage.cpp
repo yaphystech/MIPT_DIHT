389e348167d9f8be643d9f26c7487a041d699847
yaphystech <37752053+yaphystech@users.noreply.github.com>
2019-07-04 17:52:19 +0300

    Update Arbitrage.cpp

--- Arbitrage.cpp
+++ Arbitrage.cpp
@@ -8,39 +8,30 @@ using std::pair;
 class Arcgraph {
 public:
     Arcgraph() {}
-
     ~Arcgraph() {}
-
-
-    void AddEdge(const int from, const int to, const double cost) {
-        existent_vertices.insert(from);
-        adjacency_list.push_back(Edge(from, to, cost));
-    }
-
-    int VerticesCount() const {
-        return existent_vertices.size();
-    }
-
-    void GetNextVertices(int vertex, std::vector<pair<int, double >> &vertices) const;
-
-    void GetPrevVertices(int vertex, std::vector<pair<int, double >> &vertices) const;
-
+    void AddEdge(const int& from, const int& to, const double& cost);
+    int VerticesCount() const;
     friend bool ford_bellman(const Arcgraph& graph);
-
 private:
-
     struct Edge{
         int from;
         int to;
         double cost;
-
         Edge(const int& from_, const int& to_, const double& cost_):
             from(from_), to(to_), cost(cost_) {}
     };
-
     std::vector<Edge> adjacency_list;
     std::set<int> existent_vertices;
 };
+
+int Arcgraph::VerticesCount() const {
+    return existent_vertices.size();
+}
+
+void Arcgraph::AddEdge(const int& from, const int& to, const double& cost) {
+    existent_vertices.insert(from);
+    adjacency_list.push_back(Edge(from, to, cost));
+}
 
 bool ford_bellman(const Arcgraph& graph) {
     int amount_of_vertices = graph.VerticesCount();
