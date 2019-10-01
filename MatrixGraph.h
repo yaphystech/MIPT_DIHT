bc220a724c96e749bf1b650e6ee5cd07a0ea3790
yaphystech <37752053+yaphystech@users.noreply.github.com>
2019-10-01 17:08:37 +0300

    Create MatrixGraph.h

---
+++ MatrixGraph.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "IGraph.h"
+
+class MatrixGraph : public IGraph{
+private:
+    vector<vector<int>> graph;
+public:
+    MatrixGraph(const IGraph* g);
+    MatrixGraph(int numbOfVertices);
+    ~MatrixGraph();
+    int VerticesCount() const;
+    void AddEdge(int from, int to);
+    void GetNextVertices(int vertex, vector<int>& vertices) const;
+    void GetPrevVertices(int vertex, vector<int>& vertices) const;
+    void Print ();
+};
