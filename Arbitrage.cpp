459d144f39dc9f81e15889b621faaab4f0c22d31
yaphystech <37752053+yaphystech@users.noreply.github.com>
2019-10-07 08:45:36 +0300

    Update Arbitrage.cpp

--- Arbitrage.cpp
+++ Arbitrage.cpp
@@ -11,32 +11,32 @@ struct Edge{
     Edge(const int& from, const int& to, const double& cost): from(from), to(to), cost(cost) {};
 };
 
-class Arcgraph {
+class ArcGraph {
 private:
     vector<Edge> adjacencyList;
     set<int> existentVertices;
 public:
-    Arcgraph() {}
-    ~Arcgraph() {}
+    ArcGraph() {}
+    ~ArcGraph() {}
     void AddEdge(int& from, int& to, const double& cost);
     int VerticesCount() const;
     const vector<Edge>& GetAdjacencyList() const;
 };
 
-const vector<Edge>& Arcgraph::GetAdjacencyList() const{
+const vector<Edge>& ArcGraph::GetAdjacencyList() const{
     return adjacencyList;
 }
 
-int Arcgraph::VerticesCount() const {
+int ArcGraph::VerticesCount() const {
     return existentVertices.size();
 }
 
-void Arcgraph::AddEdge(int& from, int& to, const double& cost) {
+void ArcGraph::AddEdge(int& from, int& to, const double& cost) {
     existentVertices.insert(from);
     adjacencyList.push_back(Edge(from, to, cost));
 }
 
-bool FordBellman(const Arcgraph &graph) {
+bool FordBellman(const ArcGraph &graph) {
     int amountOfVertices = graph.VerticesCount();
     vector<double> distance(amountOfVertices, INT32_MIN);
     distance[0] = 1;
@@ -60,13 +60,15 @@ bool FordBellman(const Arcgraph &graph) {
 int main() {
     int amountOfVertices;
     cin >> amountOfVertices;
-    Arcgraph graph;
+    ArcGraph graph;
     for(int i = 0; i < amountOfVertices; ++i) {
         for (int j = 0; j < amountOfVertices; ++j) {
             double cost;
             if(i != j) {
                 cin >> cost;
-                graph.AddEdge(i, j, cost);
+                if (cost != -1) {
+                    graph.AddEdge(i, j, cost);
+                }
             } else {
                 graph.AddEdge(i, j, 1);
             }
